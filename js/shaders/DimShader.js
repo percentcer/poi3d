73e76b0e2bc426017c4f22f473d9c164e0b44710
percentcer <spence.miller@gmail.com>
2013-09-19 18:18:12 -0700

    something happened.  getting super weird values at .995 but seeing what I'd expect to see at .9995 (red) and .95 (green)

--- js/shaders/DimShader.js
+++ js/shaders/DimShader.js
@@ -36,23 +36,19 @@ THREE.DimShader = {
 		"uniform sampler2D prevTDiffuse;",
 		"uniform float scaleRate;",
 
-		"bool didHit;",
-
 		"vec4 forcedDifferentiation( vec4 old, vec4 new ) {",
 
-			"ivec4 iold = ivec4( old * 255.0 );",
+			"ivec4 iold = ivec4( ceil( old * 255.0 ) );",
 
-			"ivec4 inew = ivec4( new * 255.0 );",
+			"ivec4 inew = ivec4( ceil( new * 255.0 ) );",
 
-			"if ( any( equal( iold, inew ) ) ) {",
+			"if ( all( equal( iold.rgb, inew.rgb ) ) ) {",
 
-				"didHit = true;",
-
-				"return vec4( ivec4( inew.rgb - ivec3(1), inew.a ) ) / 255.0;",
+				"return ( vec4( ivec4( inew.rgb - ivec3(2), inew.a ) ) / 255.0 ) * vec4(1.0, 0.0, 0.0, 1.0);",
 
 			"} else {",
 
-				"return new;",
+				"return new * vec4(0.0, 1.0, 0.0, 1.0);",
 
 			"}",
 
@@ -60,16 +56,11 @@ THREE.DimShader = {
 
 		"void main() {",
 
-			"didHit = false;",
-
 			"vec4 texel = texture2D( prevTDiffuse, vUv );",
 
 			"vec4 nextTexel = vec4( texel.rgb * scaleRate, texel.a );",
 
 			"vec4 result    = forcedDifferentiation( texel, nextTexel );",
-
-			// debugging
-			"if (didHit) { result *= vec4(0.0, 1.0, 0.0, 1.0); }",
 
 			"gl_FragColor   = result;",
 
